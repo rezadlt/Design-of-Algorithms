8320ffbcefd963b82f7f04945111d62594ca5c41
rezadlt <46085788+rezadlt@users.noreply.github.com>
2023-11-01 20:20:36 +0330

    Add files via upload

---
+++ HW3/escape.cpp
@@ -0,0 +1,123 @@
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+typedef long double DOUBLE;
+typedef vector<DOUBLE> VD;
+typedef vector<VD> VVD;
+typedef vector<int> VI;
+
+const DOUBLE EPS = 1e-9;
+
+struct LPSolver {
+  int m, n;
+  VI B, N;
+  VVD D;
+
+  LPSolver(const VVD &A, const VD &b, const VD &c) :
+    m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, VD(n + 2)) {
+    for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) D[i][j] = A[i][j];
+    for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i]; }
+    for (int j = 0; j < n; j++) { N[j] = j; D[m][j] = -c[j]; }
+    N[n] = -1; D[m + 1][n] = 1;
+  }
+
+  void Pivot(int r, int s) {
+    double inv = 1.0 / D[r][s];
+    for (int i = 0; i < m + 2; i++) if (i != r)
+      for (int j = 0; j < n + 2; j++) if (j != s)
+        D[i][j] -= D[r][j] * D[i][s] * inv;
+    for (int j = 0; j < n + 2; j++) if (j != s) D[r][j] *= inv;
+    for (int i = 0; i < m + 2; i++) if (i != r) D[i][s] *= -inv;
+    D[r][s] = inv;
+    swap(B[r], N[s]);
+  }
+
+  bool Simplex(int phase) {
+    int x = phase == 1 ? m + 1 : m;
+    while (true) {
+      int s = -1;
+      for (int j = 0; j <= n; j++) {
+        if (phase == 2 && N[j] == -1) continue;
+        if (s == -1 || D[x][j] < D[x][s] || D[x][j] == D[x][s] && N[j] < N[s]) s = j;
+      }
+      if (D[x][s] > -EPS) return true;
+      int r = -1;
+      for (int i = 0; i < m; i++) {
+        if (D[i][s] < EPS) continue;
+        if (r == -1 || D[i][n + 1] / D[i][s] < D[r][n + 1] / D[r][s] ||
+          (D[i][n + 1] / D[i][s]) == (D[r][n + 1] / D[r][s]) && B[i] < B[r]) r = i;
+      }
+      if (r == -1) return false;
+      Pivot(r, s);
+    }
+  }
+
+  DOUBLE Solve(VD &x) {
+    int r = 0;
+    for (int i = 1; i < m; i++) if (D[i][n + 1] < D[r][n + 1]) r = i;
+    if (D[r][n + 1] < -EPS) {
+      Pivot(r, n);
+      if (!Simplex(1) || D[m + 1][n + 1] < -EPS) return -numeric_limits<DOUBLE>::infinity();
+      for (int i = 0; i < m; i++) if (B[i] == -1) {
+        int s = -1;
+        for (int j = 0; j <= n; j++)
+          if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) s = j;
+        Pivot(i, s);
+      }
+    }
+    if (!Simplex(2)) return numeric_limits<DOUBLE>::infinity();
+    x = VD(n);
+    for (int i = 0; i < m; i++) if (B[i] < n) x[B[i]] = D[i][n + 1];
+    return D[m][n + 1];
+  }
+};
+
+int main()
+{
+    int k;
+    double M,E,F,T,m,e,f,t;
+    cin>> k >> M >> E >> F;
+    VVD A;
+    VD m1,e1,f1,t1,b,c,sol;
+    
+    for(int i = 0; i < k; ++i)
+    {
+        cin >> k >>t;
+        t1.push_back(-t);
+        c.push_back(-2);
+        while (k--) {
+            cin >> m >> e >> f;
+            m1.push_back(-m);
+            e1.push_back(-e);
+            f1.push_back(-f);
+            c.push_back(-1);
+        }
+    }
+    
+/*    for(int i = 0; i < k; ++i)
+    {
+        cin>>m>>e>>f;
+        m1.push_back(+m);
+        e1.push_back(+e);
+        f1.push_back(-f);
+        c.push_back(-1);
+    }
+*/
+    A.push_back(m1);
+    A.push_back(e1);
+    A.push_back(f1);
+    A.push_back(t1);
+    b.push_back(-M);
+    b.push_back(-E);
+    b.push_back(-F);
+    b.push_back(-T);
+    LPSolver solver(A, b, c);
+    cout << setprecision (2) << fixed << -1 * solver.Solve(sol);
+    return 0;
+}
